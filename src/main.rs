a773c210ef8c5d8d33bb01ed9e239a7eb0f7ae90
jqms <hanjiangxueyesijing@gmail.com>
2026-04-18 18:01:39 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@
 
 use core::panic::PanicInfo;
 
-static GREETING: &[u8] = b"Hello World from Rust Kernel!";
+// static GREETING: &[u8] = b"Hello World from Rust Kernel!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in GREETING.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;  // 白色高亮
-        }
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    //
+    // for (i, &byte) in GREETING.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;  // 白色高亮
+    //     }
+    // }
 
     loop {}
 }
