e86b0810e8be15973f77273196af46811e959a47
lingllqs <lingllqs@163.com>
2026-04-25 23:47:02 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-// static GREETING: &[u8] = b"Hello World from Rust Kernel!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-    //
-    // for (i, &byte) in GREETING.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0x0f;  // 白色高亮
-    //     }
-    // }
+    vga_buffer::print_something();
 
     loop {}
 }
